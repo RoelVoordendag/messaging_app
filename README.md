5acf770d5072ae7826cf3b9bc477bbb431941b40
roel <roel@weticket.com>
2024-08-22 22:23:58 +0200

    Updated tauri versions discarded old stuff because there where no changes in there

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## Todo:
-- [ ] Send images to api
-- [ ] Collect images from API
-- [ ] Style the application a bit
-- [ ] A pre login screen so we can collect old messaged based on user name
-- [ ] Research on how we can setup multiple images
-- [ ] Decide on name  
+# Tauri + React + Typescript
+
+This template should help get you started developing with Tauri, React and Typescript in Vite.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use log::{ info };
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn send_message(message: &str) -> String { 
-    format!("Hello, {}! You've been greeted from the BE!", message)
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![send_message])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
