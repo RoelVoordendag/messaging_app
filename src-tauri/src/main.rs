0f5a96b15a8c6bb56969d617cf590789a3f7d9bb
roel <roel@weticket.com>
2024-08-26 22:26:00 +0200

    Started with logging user on putting in username

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+use dotenv::dotenv;
+use reqwest::Client;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn login_user()  {
+    // @todo can we move this to a auto start?
+    dotenv().ok();
+
+    let server_url = env::var("SERVER_URL").expect("The config is not correct");
+
+    let client = Client::new();
+
+    let res = client.post(format!("{}/", server_url));
+
+    /**
+     * @todo
+     *  Do API call to /api/users/create
+     *  with body:
+     *  {
+           "name": "XXX"
+        }
+     * 
+     * Check if we get response back and then go to next screen
+     */
+
+    println!("Message from Russt: {}", server_url);
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![login_user])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
+  const [loginMsg, setLoginMsg] = useState("");
   const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  async function login() {
+    setLoginMsg(`Hi ${name} we will try to log you in right now...`);
+
+    await invoke('login_user');
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>Welcome to Messaging App!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <p>Fill in your username to create or start with your first apps</p>
 
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          login();
         }}
       >
         <input
@@ -42,10 +30,10 @@ function App() {
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Login</button>
       </form>
 
-      <p>{greetMsg}</p>
+      <p>{loginMsg}</p>
     </div>
   );
 }
